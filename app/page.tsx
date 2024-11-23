c59a61e2bf0491df15db0ab4efa4f4a96502b3ce
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2024-11-23 15:00:25 +0330

    initializing

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import Image from "next/image";
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -12,33 +13,30 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
+          <Link
+            className="rounded-full  border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground  text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 pr-4 sm:pr-5"
+            style={{
+              backgroundImage: 'url(/react-query.png)',
+              backgroundOrigin: 'right',
+              backgroundSize: '220px',
+              backgroundRepeat: 'no-repeat',
+              backgroundBlendMode: 'overlay',
+            }}
+            href="/react-query"
+            // target="_blank"
             rel="noopener noreferrer"
           >
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
+              className="dark:invert rounded-e-none"
+              src="/react-query.png"
               alt="Vercel logomark"
-              width={20}
-              height={20}
+              width={45}
+              height={45}
             />
-            Deploy now
-          </a>
+            React Query
+          </Link>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -97,5 +95,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  );
+  )
 }

251fa74cc520ce531691aad1af34b0e7eab4239d
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2024-11-30 00:35:17 +0330

    category filter

--- app/react-query/components/products.tsx
+++ app/react-query/components/products.tsx
@@ -13,9 +13,12 @@ import { useQueries, useQuery } from '@tanstack/react-query'
 import { formatDistance } from 'date-fns'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import { useQueryState } from 'nuqs'
-import { ChangeEvent } from 'react'
+import { ChangeEvent, useState } from 'react'
 import { useDebouncedCallback } from 'use-debounce'
 import Product from './product'
+
+import { Button } from '@/components/ui/button'
+import { motion, AnimatePresence } from 'framer-motion'
 export interface FakerType {
   id: number
   title: string
@@ -88,13 +91,19 @@ export const BASE_URL = 'https://dummyjson.com' //https://fakestoreapi.com/
 // export const BASE_URL = 'https://api.escuelajs.co/api/v1'
 
 // https://nextjs.org/learn/dashboard-app/adding-search-and-pagination
-async function getData(sort: string | null, search: string | null) {
+async function getData(
+  sort: string | null,
+  search: string | null,
+  activeCategory: string | null
+) {
   // const url = `${BASE_URL}/products?sortBy=price&order${sort}/search?q=${search}`
   let url
   if (search) {
     url = `${BASE_URL}/products/search?q=${search}`
   } else if (sort) {
     url = `${BASE_URL}/products?sortBy=price&order${sort}`
+  } else if (activeCategory) {
+    url = `${BASE_URL}/products/${activeCategory}`
   } else {
     url = `${BASE_URL}/products`
   }
@@ -128,6 +137,7 @@ function Products() {
   const pathname = usePathname()
   const { replace } = useRouter()
   // const searchRef = useRef()
+  const [activeCategory, setActiveCategory] = useState(null)
   const search = searchParams.get('search')
   // console.log({ pathname })
   // console.log({ searchParams })
@@ -160,9 +170,9 @@ function Products() {
     return useQueries({
       queries: [
         {
-          queryKey: ['repos', sort, search],
+          queryKey: ['repos', sort, search, activeCategory],
           //Its getData NOT getData()
-          queryFn: () => getData(sort, search),
+          queryFn: () => getData(sort, search, activeCategory),
           staleTime: 5000, // 5 seconds
           gcTime: 3000, // 3 seconds instead of default 5 min
           // refetchInterval: 5000,
@@ -197,8 +207,8 @@ function Products() {
     })
   }
   const { data, categories, isError, isPending, dataUpdatedAt } = useRepos()
-  console.log({ categories })
-  console.log({ dataUpdatedAt })
+  // console.log({ activeCategory })
+  // console.log({ dataUpdatedAt })
 
   const handleSearch = useDebouncedCallback(
     (term: ChangeEvent<HTMLInputElement>) => {
@@ -231,33 +241,53 @@ function Products() {
     <div className="bg-white">
       <div className=" flex flex-col gap-8  mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="sr-only">Products</h2>
-        <article className="flex flex-col gap-8 md:flex-row items-center justify-evenly  ">
-          <Input
-            placeholder="Search..."
-            // ref={searchRef}
-            // value={search || ''}
-            // onChange={(e) => setSearch(e.target.value)}
-            onChange={handleSearch}
-            defaultValue={searchParams.get('search')?.toString()}
-          />
-          <Select
-            value={sort || 'asc'}
-            onValueChange={(e) => {
-              setSort(e)
-            }}
-          >
-            <SelectTrigger className="w-[180px]">
-              <SelectValue placeholder="Select an order" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectLabel>Order</SelectLabel>
-                <SelectItem value="asc">asc</SelectItem>
-                <SelectItem value="desc">desc</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </article>
+        <section className="flex flex-col w-full h-full">
+          <article className="flex flex-col gap-8 md:flex-row items-center justify-evenly  ">
+            <Input
+              placeholder="Search..."
+              // ref={searchRef}
+              // value={search || ''}
+              // onChange={(e) => setSearch(e.target.value)}
+              onChange={handleSearch}
+              defaultValue={searchParams.get('search')?.toString()}
+            />
+            <Select
+              value={sort || 'asc'}
+              onValueChange={(e) => {
+                setSort(e)
+              }}
+            >
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Select an order" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Order</SelectLabel>
+                  <SelectItem value="asc">asc</SelectItem>
+                  <SelectItem value="desc">desc</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </article>
+          <article className="py-8 flex flex-wrap gap-4 items-center justify-center w-full ">
+            {/* {getRandomElements(categories, 5).map((category: any) => ( */}
+            {categories.slice(0, 5).map((category: any) => (
+              <Button
+                variant={
+                  activeCategory === category.name ? 'default' : 'outline'
+                }
+                onClick={() =>
+                  activeCategory !== category.name
+                    ? setActiveCategory(category.name)
+                    : setActiveCategory(null)
+                }
+                key={category.name}
+              >
+                {category.name}
+              </Button>
+            ))}
+          </article>
+        </section>
         <h2>
           {data.products.length} Products, last update at:{' '}
           {formatDistance(dataUpdatedAt[0], new Date(), {
@@ -265,11 +295,25 @@ function Products() {
             includeSeconds: true,
           })}
         </h2>
-        <div className="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8">
-          {data.products.map((repo: FakerType) => (
-            <Product key={repo.id} product={repo} />
-          ))}
-        </div>
+        <AnimatePresence mode="wait">
+          <div className="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8">
+            {data.products.map((repo: FakerType, i: number) => (
+              <motion.div
+                initial={{ opacity: 0, x: i % 2 === 0 ? 100 : -100 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                exit={{ opacity: 0, x: i % 2 === 0 ? -100 : 100 }}
+                transition={{
+                  duration: 0.5,
+                  delayChildren: 0.5,
+                  staggerChildren: 0.5,
+                }}
+                key={repo.id}
+              >
+                <Product product={repo} />
+              </motion.div>
+            ))}
+          </div>
+        </AnimatePresence>
       </div>
     </div>
   )
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,12 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function getRandomElements<T>(array: T[], count: number): T[] {
+  const copiedArray = [...array] // Create a shallow copy of the array
+  const shuffledArray = copiedArray.sort(() => 0.5 - Math.random())
+  return shuffledArray.slice(0, count)
 }
